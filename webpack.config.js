95971e4bfbf9fc25f797f77c38061ddbe87eedd8
Dmitry Kozachek <dmitry.kozachek@gmail.com>
2020-04-17 17:28:56 +0300

    feat(modules): Add modules config to theme

--- webpack.config.js
+++ webpack.config.js
@@ -1,43 +1,47 @@
+// You can extend default webpack build here.
+// Read more in docs: https://github.com/DivanteLtd/vue-storefront/blob/master/docs/guide/core-themes/webpack.md
+const merge = require('webpack-merge');
 const themeRoot = require('@vue-storefront/core/build/theme-path');
 
-/**
-* @param {object} loader
-*/
-function addPlugins (loader) {
-  if(!loader) return
+function addPostCSSPlugin (loader, plugin) {
+  const plugins = loader.options.plugins instanceof Function
+    ? loader.options.plugins()
+    : loader.options.plugins;
 
-  let options ={
-    ident: 'postcss',
-    plugins: (loader) => [
-      require('tailwindcss')(`${themeRoot}/tailwind.config.js`),
-      require('postcss-flexbugs-fixes'),
-      require('autoprefixer')({
-        flexbox: 'no-2009'
-      })
-    ]
-  };
+  plugins.push(plugin);
 
-  loader.options = options;
+  loader.options.plugins = loader.options.plugins instanceof Function
+    ? () => plugins
+    : plugins;
 }
 
 /**
+ * Traverses webpack's module rules to find all PostCSS loaders. For each PostCSS loader
+ * the 'postcss-flexbugs-fixes' plugin has to be found and it has to be configured to skip
+ * @storefront-ui library from being processed.
+ *
  * @param {object} rules
  */
-function addPostCSSPlugins (rules) {
+function fixPostCSSPlugins (rules) {
   const processedLoaders = [];
 
   rules.forEach(rule => {
     const loader = rule.use ? rule.use.find(item => item.loader === 'postcss-loader') : null;
 
-    if (!loader && processedLoaders.includes(loader)) return
-
-    addPlugins(loader);
-    processedLoaders.push(loader);
+    if (loader && !processedLoaders.includes(loader)) {
+      addPostCSSPlugin(loader, require('tailwindcss')(`${themeRoot}/tailwind.config.js`));
+      processedLoaders.push(loader);
+    }
   });
 }
 
-module.exports = function (config, { isClient, isDev }) {
-  addPostCSSPlugins(config.module.rules);
+module.exports = function (config, { isClient }) {
+  const mergedConfig = merge(
+    { resolve: { alias: { 'src/modules/client': `${themeRoot}/config/modules` } } },
+    config
+  );
 
-  return config;
-}
+  fixPostCSSPlugins(mergedConfig.module.rules);
+
+  return mergedConfig;
+};
